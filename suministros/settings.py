3590eb9c5e462d269d9739b6b391929b7744e7d9
nenriquezc <nenriquezc@gmail.com>
2025-10-26 16:01:09 +0000

    terminado version final

--- suministros/settings.py
+++ suministros/settings.py
@@ -135,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"  # carpeta destino de collectstatic
 # Si tienes la carpeta static en el mismo nivel que manage.py:
 STATICFILES_DIRS = [
     BASE_DIR / "static",
